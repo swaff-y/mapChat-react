b3ffcb71656bbbe58ec26cf770925b9a1fd2175b
Kyle Swaffield <kyleswaffield@MacBook-Pro.local>
2021-02-03 09:48:04 +1100

    added-message-wrapping-on-map-icons

--- src/map/MapContainer.js
+++ src/map/MapContainer.js
@@ -17,14 +17,47 @@ const MapContainer = (props) => {
     setLoaded(true);
   },[])
 
-  console.log("The Points: ", points, props.messages);
+  let msg = "";
+  if(props.messages[0] !== undefined){
+      msg = props.messages[0].message
+  }
 
-  // const points = [
-  //   { lat: 34.02, lng: -82.01 },
-  //   { lat: 42.03, lng: -77.02 },
-  //   { lat: 35.03, lng: -80.04 },
-  //   { lat: 44.05, lng: -70.02 }
-  // ]
+  const str = wordWrap(msg, 14);
+
+  // console.log("The Points: ", str.split('*'));
+
+  function wordWrap(str, maxWidth) {
+      let newLineStr = "*";
+      let done = false;
+      let res = '';
+      while (str.length > maxWidth) {
+          let found = false;
+          // Inserts new line at first whitespace of the line
+          for (i = maxWidth - 1; i >= 0; i--) {
+              if (testWhite(str.charAt(i))) {
+                  res = res + [str.slice(0, i), newLineStr].join('');
+                  str = str.slice(i + 1);
+                  found = true;
+                  break;
+              }
+          }
+          // Inserts new line at maxWidth position, the word is too long to wrap
+          if (!found) {
+              res += [str.slice(0, maxWidth), newLineStr].join('');
+              str = str.slice(maxWidth);
+          }
+
+      }
+
+      return res + str;
+  }
+
+  function testWhite(x) {
+      var white = new RegExp(/^\s$/);
+      return white.test(x.charAt(0));
+  };
+
+
   const bounds = new props.google.maps.LatLngBounds();
   for (var i = 0; i < points.length; i++) {
     bounds.extend(points[i]);
@@ -53,10 +86,10 @@ const MapContainer = (props) => {
                   lng: message.longitude
                 }}
                 onClick={() => console.log("You clicked me!")}
-                title={"$100"}
-                name={"location.name"}
+                title={wordWrap(message.message,14).split("*")[0]}
+                name={message.name}
                 icon={{
-                  url: `https://chart.googleapis.com/chart?chst=d_fnote&chld=thought|1|0088FF|h|${message.message}|another line|one more line`,
+                  url: `https://chart.googleapis.com/chart?chst=d_fnote&chld=thought|1|0088FF|h|${wordWrap(message.message,14).split("*")[0]}|${wordWrap(message.message,14).split("*")[1] !== undefined ? wordWrap(message.message,14).split("*")[1] : ""}|${wordWrap(message.message,14).split("*")[2] !== undefined ? wordWrap(message.message,14).split("*")[2] : ""}`,
                 }}
                 />
       })
@@ -83,3 +116,22 @@ const MapContainer = (props) => {
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyAW5MNODxdAncbpnSGtOIl6Gyfjo-e6w3g'
 })(MapContainer);
+
+
+
+
+
+// let msg;
+// msg = props.messages[0].message
+//
+// const wrap = (s) => s.replace(
+//   /(?![^-]{1,32}$)([^-]{1,32})\s/g, '$1\n'
+// );
+//
+//  let str = wrap(msg)
+//
+//  let arr = [];
+//
+//  arr = str.split("\n");
+//
+// console.log("The string:", str, arr);
